b7335be216267f917cb27ea91fb3d624861a1a72
GYUMINSHIN <92241822+GYUMINSHIN@users.noreply.github.com>
2022-11-27 17:03:04 +0900

    refactor: calendar 관련 기능 router 폴더로 이동

--- main.js
+++ main.js
@@ -15,41 +15,17 @@ app.use(express.static('public'));
 
 //import router/signup.js
 const signup = require("./router/signup");
-app.use('/signup', signup.router);
+app.use('/signup', signup);
 
 //import router/signup.js
 const login = require("./router/login");
-app.use('/', login.router);
+app.use('/', login);
 
-//calender.html 전송
-app.get('/calendar', (req, res) => { res.sendFile(__dirname + '/public/html/calendar.html') });	
+//import router/signup.js
+const calendar = require("./router/calendar");
+app.use('/calendar', calendar);
 
 //신호 대기
 app.listen(port, () => {
 	console.log("app started");
-})
-
-app.get("/events", (req, res) => {
-	const events = [
-		{
-			title: "Event 1",
-			start: "2022-11-05", 
-			end: "2022-11-08",
-			descriptopm: "example 1"
-		},
-	  	{
-			title: "Event 2",
-			start: "2022-11-15", 
-			end: "2022-12-02",
-			descriptopm: "example 2"
-	  	},
-	  	{
-			title: "Event 3",
-			start: "2022-12-08",
-			end: "2022-12-10",
-			descriptopm: "example 3"
-	  	},
-	];
-  
-	res.json(events);
-  });
+});
---
+++ router/calendar.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+//event 데이터 보관용 배열 import
+let event = require("../data/event");
+let eventData = event;
+
+router.use((req, res, next) => {
+  next();
+});
+
+//login.html 전송
+router.get('/', (req, res) => { res.sendFile(path.join(__dirname + '/../public/html/calendar.html')) });
+
+//로그인 기능 (userData와 대조)
+router.get("/events", (req, res) => {
+	res.json(eventData.events);
+})
+
+module.exports = router;
